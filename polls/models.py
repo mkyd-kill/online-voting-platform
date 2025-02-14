128f5f8ec7b310ded44233b8144d73667679b3a2
Romeo Namunde <romeomureithi@gmail.com>
2025-02-15 01:28:13 +0300

    vote form

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from shortuuid.django_fields import ShortUUIDField
-
+from django.contrib.auth.models import User
 
 class Election(models.Model):
     id = ShortUUIDField(unique=True, max_length=5, alphabet="0123456789")
@@ -15,6 +15,6 @@ class Candidate(models.Model):
 
 class Vote(models.Model):
     id = ShortUUIDField(unique=True, max_length=5, alphabet="0123456789")
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
