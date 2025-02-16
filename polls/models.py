5ff74d8fabdf1054a0d9e4ba52cc54bd5d8ca2c5
Romeo Namunde <romeomureithi@gmail.com>
2025-02-17 01:17:23 +0300

    vote integrity and encryption

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from shortuuid.django_fields import ShortUUIDField
 from users.models import CustomUser
+import hashlib
 
 class Election(models.Model):
     id = ShortUUIDField(unique=True, length=5, max_length=5, alphabet="0123456789", primary_key=True)
@@ -30,6 +31,12 @@ class Vote(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
+    vote_hash = models.CharField(max_length=64, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        data = f"{self.user.id}{self.candidate.id}{self.timestamp}"
+        self.vote_hash = hashlib.sha256(data.encode()).hexdigest()
+        super().save(*args, **kwargs)
 
     def __repr__(self):
         return self.user.username.title()
