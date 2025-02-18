de7f12e68c8974b072669ceb28b1e54a9eee57be
Romeo Namunde <romeomureithi@gmail.com>
2025-02-18 23:29:32 +0300

    active sidebar

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import vote, results, elections
 
 urlpatterns = [
-    path("", elections, name='elections'),
+    path("elections/", elections, name='elections'),
     path('vote/<int:election_id>/', vote, name='vote'),
     path('results/<int:election_id>/', results, name='results')
 ]
