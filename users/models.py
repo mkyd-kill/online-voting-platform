22c67559e52d2c74088bff5d857ed3ad6a96ce57
Romeo Namunde <romeomureithi@gmail.com>
2025-02-24 21:24:37 +0300

    removed user profile picture

--- users/models.py
+++ users/models.py
@@ -4,4 +4,3 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     is_voter = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-    profile_img = models.ImageField(upload_to='profiles/', blank=True, null=True, default='avatar.jpg')
