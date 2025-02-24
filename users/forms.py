53725fb5c9aebe15d108a49c606c3050571b753b
Romeo Namunde <romeomureithi@gmail.com>
2025-02-24 21:25:03 +0300

    removed password field

--- users/forms.py
+++ users/forms.py
@@ -10,4 +10,4 @@ class UserForm(UserCreationForm):
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'first_name', 'last_name', 'password']
+        fields = ['username', 'first_name', 'last_name']
