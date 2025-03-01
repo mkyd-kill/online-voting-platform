2e8a9c1e1ea6b8836177819877ce4b3c24e38e6c
Romeo Namunde <romeomureithi@gmail.com>
2025-03-01 22:49:50 +0300

    default route

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register, profile
+from .views import register, profile, index
 from django.contrib.auth import views as auth
 
 urlpatterns = [
+    path("", index, name="home"),
     path('register/', register, name='register'),
     path('accounts/login/', auth.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('accounts/profile/', profile, name='profile'),
--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm, UpdateProfileForm
 
+def index(request):
+    return redirect("login")
+
 @login_required
 def profile(request):
     user = request.user
